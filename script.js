020db9593ae9848b4b76bb580901f76ed6e70600
THEBIGTOMMY13DEV <141506537+THEBIGTOMMY13DEV@users.noreply.github.com>
2024-05-21 00:57:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,146 @@
+let comedyshows = ["Young Sheldon", "The Office", "Friends", "The Big Bang Theory"];
+let actionshows = ["The Lord of the Rings: The Rings of Power", "The Rookie", "Masters of the Air"];
+let actionshowsimages = ["https://m.media-amazon.com/images/M/MV5BNzhiNjgyYmEtOThkYi00M2UyLThjNDctNjRmOGQyOTdjODhhXkEyXkFqcGdeQXVyODU2MjMyNDg@._V1_FMjpg_UX1066_.jpg","https://m.media-amazon.com/images/M/MV5BNjU1Y2JjMjYtNDJiNS00NmFhLTk2N2MtMDEwNDI1YzVhNGI1XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1080_.jpg","https://m.media-amazon.com/images/M/MV5BYzgwNTI2YjAtMDM5Zi00MTAzLThkMmUtNTEyYWY1ODA2ZDdjXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UY3000_.jpg"];
+let animationshows = ["Family Guy","The Simpsons","Rick and Morty","South Park"];
+let crimeshows = ["Breaking Bad","Law & Order: Special Victims Unit","NCIS","True Detective"];
+let dramashows = ["Game Of Thrones","","",""];
+let experimentalshows = ["","","",""];
+let fantasyshows = ["","","",""];
+let historicalshows = ["","","",""];
+let horrorshows = ["","","",""];
+let romanceshows = ["","","",""];
+let scifishows = ["","","",""];
+let thrillershows = ["","","",""];
+let westernshows = ["","","",""];
+let comedyshowsimages = ["https://m.media-amazon.com/images/M/MV5BYmQwZTFlN2YtNGI0My00NjUyLTllY2ItNzNlZmU0ZDg2NzEwXkEyXkFqcGdeQXVyMTU2MzIxNDQ5._V1_FMjpg_UX1250_.jpg","https://m.media-amazon.com/images/M/MV5BMDNkOTE4NDQtMTNmYi00MWE0LWE4ZTktYTc0NzhhNWIzNzJiXkEyXkFqcGdeQXVyMzQ2MDI5NjU@._V1_FMjpg_UY1922_.jpg","https://m.media-amazon.com/images/M/MV5BNDVkYjU0MzctMWRmZi00NTkxLTgwZWEtOWVhYjZlYjllYmU4XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX680_.jpg","https://m.media-amazon.com/images/M/MV5BY2FmZTY5YTktOWRlYy00NmIyLWE0ZmQtZDg2YjlmMzczZDZiXkEyXkFqcGdeQXVyNjg4NzAyOTA@._V1_FMjpg_UX960_.jpg"];
+let moviesontvimages = ["","","",""];
+let animationshowsimages = ["https://m.media-amazon.com/images/M/MV5BNTYwNWI3MmYtMGU3OC00M2FkLWFhZDAtNTc3ZjQ0YTIxMmQxXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_FMjpg_UX1237_.jpg","https://m.media-amazon.com/images/M/MV5BYjFkMTlkYWUtZWFhNy00M2FmLThiOTYtYTRiYjVlZWYxNmJkXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_FMjpg_UX1000_.jpg","https://m.media-amazon.com/images/M/MV5BODEwZjEzMjAtNjQxMy00Yjc4LWFlMDAtYjhjZTAxNDU3OTg3XkEyXkFqcGdeQXVyOTM2NTM4MjA@._V1_FMjpg_UY2880_.jpg","https://m.media-amazon.com/images/M/MV5BOGQ1Zjk0NDAtMDA4Ni00NGViLTgzYjItYjkzOTJhYzNjZGI5XkEyXkFqcGdeQXVyMzM4NjcxOTc@._V1_FMjpg_UY2100_.jpg"];
+let crimeshowsimages = ["https://m.media-amazon.com/images/M/MV5BYmQ4YWMxYjUtNjZmYi00MDQ1LWFjMjMtNjA5ZDdiYjdiODU5XkEyXkFqcGdeQXVyMTMzNDExODE5._V1_FMjpg_UY3000_.jpg","https://m.media-amazon.com/images/M/MV5BMjU2OWExZTAtMzg0My00NjFjLThiM2YtNmI1Y2RkY2M0ZGVkXkEyXkFqcGdeQXVyMTUwMzM5ODkz._V1_FMjpg_UX675_.jpg","https://m.media-amazon.com/images/M/MV5BMDAxYWFiZTUtNmM1My00OGMxLTlmMjAtNDViNWZhYjA5MjE1XkEyXkFqcGdeQXVyODMxODMwNjA@._V1_FMjpg_UY3000_.jpg","https://m.media-amazon.com/images/M/MV5BMjU0YWJjYzEtY2NiNy00MWJmLThmMjAtYmRmOGE5ZmUxMGYzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX810_.jpg"];
+let dramashowsimages = ["https://m.media-amazon.com/images/M/MV5BN2IzYzBiOTQtNGZmMi00NDI5LTgxMzMtN2EzZjA1NjhlOGMxXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg"];
+let experimentalshowsimages = ["","","",""];
+let fantasyshowsimages = ["","","",""];
+let historicalshowsimages = ["","","",""];
+let horrorshowsimages = ["","","",""];
+let romanceshowsimages = ["","","",""];
+let scifishowsimages = ["","","",""];
+let thrillershowsimages = ["","","",""];
+let westernshowimages = ["","","",""];
+let result = document.querySelector(".results");
+let suggestionsimages = [];
+let resetButton=document.querySelector(".button3");
+// Submit Button 
+let submitButton = document.querySelector(".button1");
+let suggestButton = document.querySelector(".button2");
+suggestButton.onclick = function() {
+    let suggestions=document.querySelector(".input2").value;
+    suggestionsimages.push(suggestions);
+    console.log(suggestionsimages);
+}; 
+submitButton.onclick = function() {
+    let genre = document.querySelector(".input1").value;
+    if (genre === "drama") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + dramashows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + dramashows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + dramashows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + dramashows[3] + "</p>");
+        for (let dramaimage of dramashowsimages) {
+            result.insertAdjacentHTML("afterBegin", "<img src='" + dramaimage + "'>");
+        }
+    }
+    
+    if (genre === "comedy") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + comedyshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + comedyshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + comedyshows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + comedyshows[3] + "</p>");
+for (let comedyimage of comedyshowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + comedyimage + "'>");
+    }
+}
+    
+    if (genre === "action") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + actionshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + actionshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + actionshows[2] + "</p>");
+for (let actionimage of actionshowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + actionimage + "'>");
+    }
+}  
+    
+    if (genre === "animation") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + animationshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + animationshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + animationshows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + animationshows[3] + "</p>");
+for (let animationimage of animationshowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + animationimage + "'>");
+    }
+}
+    
+    if (genre === "romance") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + romanceshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + romanceshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + romanceshows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + romanceshows[3] + "</p>");
+for (let romanceimage of romanceshowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + romanceimage + "'>");
+
+    }
+}
+    
+    if (genre === "scifi") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + scifishows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + scifishows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + scifishows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + scifishows[3] + "</p>");
+for (let scifiimage of scifishowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + scifiimage + "'>");
+    }
+}
+    
+    if (genre === "horror") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + horrorshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + horrorshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + horrorshows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + horrorshows[3] + "</p>");
+for (let horrorimage of horrorshowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + horrorimage + "'>");
+    }
+}
+    
+    if (genre === "historical") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + historicalshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + historicalshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + historicalshows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + historicalshows[3] + "</p>");
+for (let historicalimage of historicalshowsimages) {
+result.insertAdjacentHTML("afterBegin", "<img src='" + historicalimage + "'>");
+            }
+        }
+    
+    if (genre === "crime") {
+        result.insertAdjacentHTML("afterBegin", "<p>" + crimeshows[0] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + crimeshows[1] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + crimeshows[2] + "</p>");
+        result.insertAdjacentHTML("afterBegin", "<p>" + crimeshows[3] + "</p>");
+    for (let crimeimage of crimeshowsimages) {
+    result.insertAdjacentHTML("afterBegin", "<img src='" + crimeimage + "'>");
+                }
+            }
+    if (genre === "suggestions") {
+    for (let suggestionimage of suggestionsimages) {
+    result.insertAdjacentHTML("afterBegin", "<img src='" + suggestionimage + "'>");
+                        }
+                    } else if (genre === "suggestions" || null || ""){
+                        result.innerHTML=" ";
+                    }
+}
+resetButton.onclick = function(){
+    result.innerHTML= "";
+}
+
+const genrebutton = document.getElementById("genrebutton");
+    const disableButton = () => {
+      genrebutton.disabled = true;
+    };
+    genrebutton.addEventListener("click", disableButton);
